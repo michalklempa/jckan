3ae547d53814b046a1606e6b2a5397c87fd70a70
Michal Klempa <mkl@eea.sk>
2014-10-29 11:03:37 +0100

    Reformating

--- src/test/java/eu/comsode/libraries/jckan/ResourceTest.java
+++ src/test/java/eu/comsode/libraries/jckan/ResourceTest.java
@@ -14,10 +14,10 @@ public class ResourceTest {
         CkanRepository ckanRepository = new CkanRepository("http://localhost:5000/api/3/action/", "c0b9f02a-6aff-4329-83c3-6686e14b4b5d");
         Resource resource1 = new Resource();
         resource1.setUrl("http://google.com");
-        resource1.setName("dsaffdsa");
+        resource1.setName("bububu");
 //        Resource resource = ckanRepository.getResourceDAO().read("2337edaf-56ca-40f1-bbce-59518dd88ce4");
         Resource resource = ckanRepository.getResourceDAO().create("1stdataset", resource1, new FileUpload(new File("/home/michal/Pictures/aaa.png")));
-        ckanRepository.getResourceDAO().delete(resource.getId());
+//        ckanRepository.getResourceDAO().delete(resource.getId());
         System.out.println(resource.toString());
     }
 }
